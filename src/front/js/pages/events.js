daf026ed856f391a47c4e1d66c91b829cb3548d0
ChickenOTC <alekreed98@gmail.com>
2022-06-14 01:03:41 +0000

    Updating events

--- src/front/js/pages/events.js
+++ src/front/js/pages/events.js
@@ -1,85 +1,10 @@
 import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import ReactDOM from "react-dom";
+import { Appbar } from "../component/Appbar/Appbar";
 
 import "../../styles/events.css";
 
 export const Events = () => {
-  // Array to store month string values
-  const allMonthValues = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  // State for date selected by user
-  const [selectedDate, setSelectedDate] = useState();
-
-  // State for text above calander
-  const [calendarText, setCalendarText] = useState(`No Date is selected`);
-
-  // Function to update selected date and calander text
-  const handleDateChange = (value) => {
-    setSelectedDate(value);
-    setCalendarText(`The selected Date is ${value.toDateString()}`);
-  };
-
-  // Function to handle selected Year change
-  const handleYearChange = (value) => {
-    const yearValue = value.getFullYear();
-    setCalendarText(`${yearValue} Year  is selected`);
-  };
-
-  // Function to handle selected Month change
-  const handleMonthChange = (value) => {
-    const monthValue = allMonthValues[value.getMonth()];
-    setCalendarText(`${monthValue} Month  is selected`);
-  };
-
-  return (
-    <div className="app">
-      <h2 className="calander-details">{calendarText}</h2>
-      <Calendar
-        onClickMonth={handleMonthChange}
-        onClickYear={handleYearChange}
-        onChange={handleDateChange}
-        value={selectedDate}
-      />
-      <h1 className="header">React Calendar</h1>
-      <div>
-        <Appbar />
-        <Calendar
-          onChange={setDate}
-          value={date}
-          onClickDay={() => setShowTime(true)}
-        />
-      </div>
-
-      {date.length > 0 ? (
-        <p>
-          <span>Start:</span>
-          {date[0].toDateString()}
-          &nbsp; &nbsp;
-          <span>End:</span>
-          {date[1].toDateString()}
-        </p>
-      ) : (
-        <p>
-          <span>Default selected date:</span>
-          {date.toDateString()}
-        </p>
-      )}
-      <Times showTime={showTime} date={date} />
-    </div>
-  );
+  <div>
+    <Appbar />
+  </div>;
 };
